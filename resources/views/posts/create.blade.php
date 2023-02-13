80636369ab3fc524a1ae1b8d0057a87b14715693
Bruno <bgolombduran@cifpfbmoll.eu>
2023-02-13 17:59:15 +0100

    fix: ruta store

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <form class="bg-white p-6 rounded-lg" method="post" action="/post">
+                    <form class="bg-white p-6 rounded-lg" method="post" action="{{ route('post.store') }}">
                         @csrf
 
                         <label class="block font-medium mb-2" for="title"> {{ __('post.title') }} </label>
